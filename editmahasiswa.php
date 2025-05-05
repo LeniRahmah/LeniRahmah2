47ff7b6e865d56cf2701bfb22cf93f9b94438305
LeniRahmah <lenirahmah051004@gmal.com>
2025-05-05 16:39:04 +0800

    menambah template

--- editmahasiswa.php
+++ editmahasiswa.php
@@ -1,8 +1,8 @@
 <?php
 include "koneksi.php";
 
-$query= "SELECT * FROM prodi";
-$data= ambildata($query);
+$query = "SELECT * FROM prodi";
+$data = ambildata($query);
 
 $nim = $_GET['nim'];
 $querymahasiswa = "SELECT * FROM mahasiswa WHERE nim = '$nim'";
@@ -12,6 +12,7 @@ $datamahasiswa = ambildata($querymahasiswa);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,45 +22,43 @@ $datamahasiswa = ambildata($querymahasiswa);
 <body>
     <h1>Edit Data Mahasiswa</h1>
     <form action="editaksimahasiswa.php" method="post">
-    <table>
-        <tr>
-            <td>nim</td>
-            <td><input type="text" name="nim" value="<?= $datamahasiswa[0]['nim'] ?>"></td>
-        </tr>
-        <tr>
-            <td>nama</td>
-            <td><input type="text" name="nama" value="<?= $datamahasiswa[0]['nama'] ?>"></td>
-        </tr>
-        <tr>
-            <td>tanggal Lahir</td>
-            <td><input type="date" name="tanggallahir" value="<?= $datamahasiswa[0]['tanggallahir'] ?>"></td>
-        </tr>
-        <tr>
-            <td>telp</td>
-            <td><input type="text" name="telp" value="<?= $datamahasiswa[0]['telp'] ?>"></td>
-        </tr>
-        <tr>
-            <td>email</td>
-            <td><input type="email" name="email" value="<?= $datamahasiswa[0]['email'] ?>"></td>
-        </tr>
-        <tr>
-            <td>prodi</td>
-            <td><select name="id_prodi">
-                <?php foreach ($data as $d) : ?>
-                    <option value="<?php echo $d['id'] ?>"
-                    <?=
-                    $d['id'] == $datamahasiswa[0]['id'] ?
-                    "SELECTED" : ""
-                    ?>
-                    ><?php echo $d['nama'] ?></option>
-                <?php endforeach ?>
-            </select>
-            </td>
-        </tr> 
-    </table> 
-    <a href=
-    <a href="index.php">kembali</a>
-    <button type="submit">simpan</button>
+        <table>
+            <tr>
+                <td>nim</td>
+                <td><input type="text" name="nim" value="<?= $datamahasiswa[0]['nim'] ?>"></td>
+            </tr>
+            <tr>
+                <td>nama</td>
+                <td><input type="text" name="nama" value="<?= $datamahasiswa[0]['nama'] ?>"></td>
+            </tr>
+            <tr>
+                <td>tanggal Lahir</td>
+                <td><input type="date" name="tanggallahir" value="<?= $datamahasiswa[0]['tanggallahir'] ?>"></td>
+            </tr>
+            <tr>
+                <td>telp</td>
+                <td><input type="text" name="telp" value="<?= $datamahasiswa[0]['telp'] ?>"></td>
+            </tr>
+            <tr>
+                <td>email</td>
+                <td><input type="email" name="email" value="<?= $datamahasiswa[0]['email'] ?>"></td>
+            </tr>
+            <tr>
+                <td>prodi</td>
+                <td><select name="id_prodi">
+                        <?php foreach ($data as $d) : ?>
+                            <option value="<?php echo $d['id'] ?>"
+                                <?=
+                                $d['id'] == $datamahasiswa[0]['id'] ?
+                                    "SELECTED" : ""
+                                ?>><?php echo $d['nama'] ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </td>
+            </tr>
+        </table>
+        <a href=<a href="index.php">kembali</a>
+        <button type="submit">simpan</button>
 
     </form>
 </body>
--- index.php
+++ index.php
@@ -6,53 +6,86 @@ if (!isset($_SESSION['login'])) {
 
 include "koneksi.php";
 
-$query= "SELECT m .*, p.nama namaProdi FROM mahasiswa m JOIN prodi p ON m.id = p.id;";
-$data= ambildata($query);
+$query = "SELECT m .*, p.nama namaProdi FROM mahasiswa m JOIN prodi p ON m.id = p.id;";
+$data = ambildata($query);
 
+include "template/header.php";
+include "template/sidebar.php";
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIMPADU POLIBAN</title>
-</head>
-<body>
-    <h1>DATA MAHASISWA</h1
-    <br>
-    <a href="tambahmahasiswa.php">Tambah</a>
-    <table border="1" cellspacing="0" cellspadding="5">
-        <thead>
-            <th>No</th>
-            <th>NIM</th>
-            <th>Nama</th>
-            <th>Telp</th>
-            <th>Email</th>
-            <th>Prodi</th>
-            <th>Aksi</th>
+<!--begin::App Main-->
+<main class="app-main">
+    <!--begin::App Content Header-->
+    <div class="app-content-header">
+        <!--begin::Container-->
+        <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+                <div class="col-sm-6">
+                    <h3 class="mb-0">Dashboard</h3>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-end">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                    </ol>
+                </div>
+            </div>
+            <!--end::Row-->
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h3 class="card-title">Data Mahasiswa</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>NIM</th>
+                                    <th>Nama</th>
+                                    <th>Telp</th>
+                                    <th>Email</th>
+                                    <th>Prodi</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
 
-    </thead>
-    <tbody>
+                                <?php
+                                $i = 1;
+                                foreach ($data as $d) : ?>
+                                    <tr>
+                                        <td><?php echo $i++; ?></td>
+                                        <td><?php echo $d["nim"] ?></td>
+                                        <td><?php echo $d["nama"] ?></td>
+                                        <td><?php echo $d["telp"] ?></td>
+                                        <td><?php echo $d["email"] ?></td>
+                                        <td><?php echo $d["namaProdi"] ?></td>
+                                        <td><a class="btn btn-danger" href="deletemahasiswa.php?nim=<?= $d['nim']; ?>"
+                                                onclick="return confirm('Yakin ingin hapus?')">Delete</a>
+                                            <a class="btn btn-warning" href="editmahasiswa.php?nim=<?= $d['nim']; ?>">Edit</a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
 
-    <?php
-    $i = 1;
-    foreach ($data as $d) : ?>
-        <tr>
-           <td><?php echo $i++; ?></td>
-           <td><?php echo $d["nim"]?></td>
-           <td><?php echo $d["nama"]?></td>
-           <td><?php echo $d["telp"]?></td>
-           <td><?php echo $d["email"]?></td>
-           <td><?php echo $d["namaProdi"]?></td>
-           <td><a href="deletemahasiswa.php?nim=<?= $d['nim']; ?>" onclick="return confirm('Yakin ingin hapus?')">Delete</a> | <a href="editmahasiswa.php?nim=<?= $d['nim']; ?>">Edit</a>
-        </td>
-        </tr>
-    <?php endforeach; ?>
-    
-    </tbody>
-</table>
-<a href="logout.php">Keluar</a>
+                </div>
+                <!-- /.card -->
 
-</body>
-</html>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+
+            <!-- /.col -->
+        </div>
+
+        <?php
+        include "template/footer.php";
+        ?>
